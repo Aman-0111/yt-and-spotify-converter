4b695102bc4a443e6cf53953d28fc59486dfef2c
Aman-0111 <aman.singh.thind@outlok.com>
2025-04-13 16:28:36 +0100

    Added tabs

--- main.py
+++ main.py
@@ -5,16 +5,42 @@ import sys
 import sv_ttk
 import tkinter
 from tkinter import ttk
+from pytube import YouTube
 
 class GUI:
     def __init__(self):
         self.root = tkinter.Tk()
-        self.root.geometry("500x280")
+        self.root.geometry("400x280")
         self.tab_parent = ttk.Notebook(self.root)
 
         self.tab1 = ttk.Frame(self.tab_parent)
         self.tab2 = ttk.Frame(self.tab_parent)
         self.tab3 = ttk.Frame(self.tab_parent)
+
+        label1 = ttk.Label(self.tab1, text = "Youtube Video URL:")
+        entry1 = ttk.Entry(self.tab1)
+        button1 = ttk.Button(self.tab1, text="Convert")
+        
+        label1.grid(row=0, column=1, padx=15, pady=15)
+        entry1.grid(row=1, column=1, padx=15, pady=15)
+        button1.grid(row=2, column=1, padx=15, pady=15)
+
+        label2 = ttk.Label(self.tab2, text = "Youtube Video URL:")
+        entry2 = ttk.Entry(self.tab2)
+        button2 = ttk.Button(self.tab2, text="Convert")
+        
+        label2.grid(row=0, column=1, padx=15, pady=15)
+        entry2.grid(row=1, column=1, padx=15, pady=15)
+        button2.grid(row=2, column=1, padx=15, pady=15)
+
+        label3 = ttk.Label(self.tab3, text = "Spotify URL:")
+        entry3 = ttk.Entry(self.tab3)
+        button3 = ttk.Button(self.tab3, text="Convert")
+        
+        label3.grid(row=0, column=1, padx=15, pady=15)
+        entry3.grid(row=1, column=1, padx=15, pady=15)
+        button3.grid(row=2, column=1, padx=15, pady=15)
+
         self.tab_parent.add(self.tab1, text = "Youtube to MP4")
         self.tab_parent.add(self.tab2, text = "Youtube to MP3")
         self.tab_parent.add(self.tab3, text = "Spotify to MP3")
@@ -32,6 +58,9 @@ class GUI:
 
             self.root.wm_attributes("-alpha", 0.99)
             self.root.wm_attributes("-alpha", 1)
+
+    # def yt_to_mp4(self):
+    #     yt = YouTube.
 
 
 if __name__ == "__main__":
